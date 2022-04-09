05b0a488299e0d694e9a8889ce7457bd8945d63f
alex <alexander.jansen@studmail.w-hs.de>
2022-04-09 16:30:47 +0200

    OpenGL SDL 'Hello World'

---
+++ main.cpp
@@ -0,0 +1,45 @@
+/*
+#include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#define SDL_MAIN_HANDLED
+*/
+
+/*****************
+    only Windows
+ *****************/
+#ifdef _WIN32
+#include <SDL.h>
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "glew32s.lib")
+#pragma comment(lib, "opengl32.lib")
+
+/*****************
+    other OS
+ *****************/
+#else
+#include <SDL2/SDL.h>
+#endif
+
+
+
+int main() {
+    SDL_Window* window;
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    window = SDL_CreateWindow("C++ OpenGL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    bool close = false;
+    while (!close) {
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                close = true;
+            }
+        }
+    }
+
+    return 0;
+}
